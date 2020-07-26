d42b068497829adabea0cb98cc6dc0e5dccf8e52
gerihal <gerahernan.al@gmail.com>
2020-07-26 15:04:21 +0200

    added method return hello world

--- Controllers/EmployeesController.cs
+++ Controllers/EmployeesController.cs
@@ -10,7 +10,7 @@ using EmployeeWebAPI.Models;
 
 namespace EmployeeWebAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route ( "api/[controller]" )]
     [ApiController]
     public class EmployeesController : ControllerBase
     {
@@ -19,31 +19,33 @@ namespace EmployeeWebAPI.Controllers
 
         // Se ha pasado el contexto por el constructor mediante Inyección de dependencias que viene del Startup
         // De aqui se puede acceder al context y del Context se accede a la base de datos física
-        public EmployeesController(EmployeeWebAPIContext context)
+        public EmployeesController ( EmployeeWebAPIContext context )
         {
             _context = context;
         }
 
         // GET: api/Employees
         [HttpGet]
-        public string Get ( )
+        public async Task<ActionResult<IEnumerable<Employee>>> GetEmployee ( )
         {
-            return "HELLO WORLD";
+            return await _context.Employee.ToListAsync ();
         }
-        //public async Task<ActionResult<IEnumerable<Employee>>> GetEmployee()
+
+        //public string Get ( )
         //{
-        //    return await _context.Employee.ToListAsync();
+        //    return "HELLO WORLD";
         //}
 
-        // GET: api/Employees/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Employee>> GetEmployee(int id)
-        {
-            var employee = await _context.Employee.FindAsync(id);
 
-            if (employee == null)
+        // GET: api/Employees/5
+        [HttpGet ( "{id}" )]
+        public async Task<ActionResult<Employee>> GetEmployee ( int id )
+        {
+            var employee = await _context.Employee.FindAsync ( id );
+
+            if(employee == null)
             {
-                return NotFound();
+                return NotFound ();
             }
 
             return employee;
@@ -52,25 +54,25 @@ namespace EmployeeWebAPI.Controllers
         // PUT: api/Employees/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutEmployee(int id, Employee employee)
+        [HttpPut ( "{id}" )]
+        public async Task<IActionResult> PutEmployee ( int id, Employee employee )
         {
-            if (id != employee.EmployeeId)
+            if(id != employee.EmployeeId)
             {
-                return BadRequest();
+                return BadRequest ();
             }
 
-            _context.Entry(employee).State = EntityState.Modified;
+            _context.Entry ( employee ).State = EntityState.Modified;
 
             try
             {
-                await _context.SaveChangesAsync();
+                await _context.SaveChangesAsync ();
             }
-            catch (DbUpdateConcurrencyException)
+            catch(DbUpdateConcurrencyException)
             {
-                if (!EmployeeExists(id))
+                if(!EmployeeExists ( id ))
                 {
-                    return NotFound();
+                    return NotFound ();
                 }
                 else
                 {
@@ -78,40 +80,40 @@ namespace EmployeeWebAPI.Controllers
                 }
             }
 
-            return NoContent();
+            return NoContent ();
         }
 
         // POST: api/Employees
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
         [HttpPost]
-        public async Task<ActionResult<Employee>> PostEmployee(Employee employee)
+        public async Task<ActionResult<Employee>> PostEmployee ( Employee employee )
         {
-            _context.Employee.Add(employee);
-            await _context.SaveChangesAsync();
+            _context.Employee.Add ( employee );
+            await _context.SaveChangesAsync ();
 
-            return CreatedAtAction("GetEmployee", new { id = employee.EmployeeId }, employee);
+            return CreatedAtAction ( "GetEmployee", new { id = employee.EmployeeId }, employee );
         }
 
         // DELETE: api/Employees/5
-        [HttpDelete("{id}")]
-        public async Task<ActionResult<Employee>> DeleteEmployee(int id)
+        [HttpDelete ( "{id}" )]
+        public async Task<ActionResult<Employee>> DeleteEmployee ( int id )
         {
-            var employee = await _context.Employee.FindAsync(id);
-            if (employee == null)
+            var employee = await _context.Employee.FindAsync ( id );
+            if(employee == null)
             {
-                return NotFound();
+                return NotFound ();
             }
 
-            _context.Employee.Remove(employee);
-            await _context.SaveChangesAsync();
+            _context.Employee.Remove ( employee );
+            await _context.SaveChangesAsync ();
 
             return employee;
         }
 
-        private bool EmployeeExists(int id)
+        private bool EmployeeExists ( int id )
         {
-            return _context.Employee.Any(e => e.EmployeeId == id);
+            return _context.Employee.Any ( e => e.EmployeeId == id );
         }
     }
 }

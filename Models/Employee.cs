85fdb5c5f1af97d5d2a0507c51012b2b7a245fb9
gerihal <gerahernan.al@gmail.com>
2020-07-26 12:12:17 +0200

    added Models folder and class Employee

---
+++ Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeWebAPI.Models
+{
+    public class Employee
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Position { get; set; }
+
+        public decimal Salary { get; set; }
+    }
+}
